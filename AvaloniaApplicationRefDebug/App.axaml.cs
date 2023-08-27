dad1a4eba0034452786314747587313b00671c10
SKProCH <skproch@yandex.ru>
2023-08-27 20:44:18 +0300

    Initial commit

---
+++ AvaloniaApplicationRefDebug/App.axaml.cs
@@ -0,0 +1,23 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaApplicationRefDebug;
+
+public partial class App : Application {
+    public override void Initialize() {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted() {
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+            desktop.MainWindow = new MainWindow();
+        }
+
+        base.OnFrameworkInitializationCompleted();
+        
+        var transitioningContentControl = new TransitioningContentControl();
+        transitioningContentControl.Content = null;
+    }
+}
